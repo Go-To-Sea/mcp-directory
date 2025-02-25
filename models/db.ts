de1c8f64b1446a822eb77f1b619e490ce84355d3
LiMing <LiMing1459276281@163.com>
2025-02-25 23:05:12 +0800

    调试修改

--- models/db.ts
+++ models/db.ts
@@ -1,6 +1,8 @@
+
 import { createClient } from "@supabase/supabase-js";
 
 export function getSupabaseClient() {
+  console.log('process.env',process.env)
   const client = createClient(
     process.env.SUPABASE_URL || "",
     process.env.SUPABASE_ANON_KEY || ""
