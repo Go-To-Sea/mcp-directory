9a1a8f3f1ff71ec5b9b205dbdfc92febe9a5973f
wu xin ping <xinping.wu@gh-ca.com>
2025-02-26 11:00:29 +0800

    init

--- models/db.ts
+++ models/db.ts
@@ -4,8 +4,8 @@ import { createClient } from "@supabase/supabase-js";
 export function getSupabaseClient() {
   console.log('process.env',process.env)
   const client = createClient(
-    process.env.SUPABASE_URL || "",
-    process.env.SUPABASE_ANON_KEY || ""
+    process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
   );
 
   return client;
