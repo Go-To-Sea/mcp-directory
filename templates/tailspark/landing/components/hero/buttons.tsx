f9cc820a051267718b7e3df9420c806647bb4e85
wu xin ping <xinping.wu@gh-ca.com>
2025-02-26 11:13:35 +0800

    屏蔽github地址

--- templates/tailspark/landing/components/hero/buttons.tsx
+++ templates/tailspark/landing/components/hero/buttons.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-
-import Github from "./github";
+import React from "react";
 
 export default function () {
   return (
     <div className="my-2 md:my-6 mx-auto text-center flex">
       <div className="mx-auto flex items-center">
-        <Github owner="chatmcp" repo="mcp-directory" interval={6000} />
       </div>
     </div>
   );
