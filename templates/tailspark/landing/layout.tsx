78a32a2cade778f43f0bf7e206b6cf0689d9a2a2
LiMing <LiMing1459276281@163.com>
2025-03-09 22:47:30 +0800

    update build

--- templates/tailspark/landing/layout.tsx
+++ templates/tailspark/landing/layout.tsx
@@ -7,6 +7,7 @@
  */
 import "./assets/style.css";
 
+
 import Footer from "./components/footer";
 import Header from "./components/header";
 import BackToTop from "./components/backToTop/backToTop";
