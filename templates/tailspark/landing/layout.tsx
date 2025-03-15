4d2f1bf13df17d058906c3441f4c928e1502b109
LiMing <LiMing1459276281@163.com>
2025-03-15 18:01:20 +0800

    添加google meta和 google js

--- templates/tailspark/landing/layout.tsx
+++ templates/tailspark/landing/layout.tsx
@@ -6,8 +6,7 @@
  * @LastEditTime: 2025-03-11 00:53:26
  */
 import "./assets/style.css";
-
-
+import Script from 'next/script';
 import Footer from "./components/footer";
 import Header from "./components/header";
 import BackToTop from "./components/backToTop/backToTop";
@@ -18,16 +17,40 @@ export default function ({
   page,
 }: Readonly<{
   children: React.ReactNode;
-  page:any;
+  page: any;
 }>) {
   return (
-    <main className="bg-background text-foreground ">
-      <div className="border-b-2">
-        {page.header && <Header header={page.header} />}
-      </div>
-      {children}
-      {/* {page.footer && <Footer footer={page.footer} />} */}
-      <BackToTop />
-    </main>
+    <>
+      {/* Google Analytics 代码 */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-2RH32YK2JB"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          console.log('Google Analytics === 加载完成');
+          gtag('config', 'G-2RH32YK2JB');
+        `}
+      </Script>
+
+      {/* Google AdSense 代码 */}
+      <Script
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9486334752310533"
+        strategy="afterInteractive"
+        crossOrigin="anonymous"
+      />
+
+      <main className="bg-background text-foreground ">
+        <div className="border-b-2">
+          {page.header && <Header header={page.header} />}
+        </div>
+        {children}
+        {/* {page.footer && <Footer footer={page.footer} />} */}
+        <BackToTop />
+      </main>
+    </>
   );
 }
