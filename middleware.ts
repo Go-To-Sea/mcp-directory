a7564a9bf5ebbb40f1b0084ed7c4e766811f7fe8
LiMing <LiMing1459276281@163.com>
2025-03-24 09:25:54 +0800

    fix bug

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,8 @@ import createMiddleware from 'next-intl/middleware';
 // next-intl 4.x 版本的正确配置
 export default createMiddleware({
   locales: ['en', 'zh', 'ja', 'ko'],
-  defaultLocale: 'en'
+  defaultLocale: 'en',
+  localeDetection: false
 });
  
 export const config = {
